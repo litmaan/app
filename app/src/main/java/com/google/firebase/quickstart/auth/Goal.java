5021b63c7d1f33cd7e66b0aa5165bf53050f85ef
MP <mateuszp181@gmail.com>
2018-03-04 20:04:47 +0100

    Poprawiona walidacja

--- app/src/main/java/com/google/firebase/quickstart/auth/Goal.java
+++ app/src/main/java/com/google/firebase/quickstart/auth/Goal.java
@@ -51,14 +51,22 @@ public class Goal extends AppCompatActivity {
         public void onClick(View v) {
             String reg = "([0-9]+)";
             String ageString = age.getText().toString();
-            String widthString = weight.getText().toString();
+            String weightString = weight.getText().toString();
             String heightString = height.getText().toString();
-            if (ageString.isEmpty() || widthString.isEmpty() || heightString.isEmpty()) {
+            if (ageString.isEmpty() || weightString.isEmpty() || heightString.isEmpty()) {
                 Toast.makeText(Goal.this, "Wszystkie pola muszą być uzupełnione", Toast.LENGTH_SHORT).show();
-            }else if (!ageString.matches(reg) || !widthString.matches(reg) || !heightString.matches(reg)) {
+            }else if (!ageString.matches(reg) || !weightString.matches(reg) || !heightString.matches(reg)) {
                 Toast.makeText(Goal.this, "Podaj poprawne dane", Toast.LENGTH_SHORT).show();
 
-            }else {
+            }else if(Integer.valueOf(ageString) >150){
+                Toast.makeText(Goal.this,"Podany wiek jest za wysoki",Toast.LENGTH_SHORT).show();
+            }else if(Integer.valueOf(weightString) > 200){
+                Toast.makeText(Goal.this,"Podana waga jest za duża",Toast.LENGTH_SHORT).show();
+            }else if(Integer.valueOf(heightString) > 150){
+                Toast.makeText(Goal.this,"Podany wzrost jest za duży",Toast.LENGTH_SHORT).show();
+            }
+
+            else {
                 CurrentUser user = new CurrentUser(Integer.valueOf(age.getText().toString()), Double.valueOf(weight.getText().toString()),
                         Double.valueOf(height.getText().toString()), activity.getSelectedItem().toString(), sex.getSelectedItem().toString());
                 RealtimeDatabase rd = new RealtimeDatabase();
